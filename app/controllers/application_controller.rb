a5d49bc39742855a97fc7df10fa350d982460240
Gaurank Sharma <gaurank.sharma@sparkeighteen.com>
2021-05-04 14:44:20 +0530

    user created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
-end
+    protect_from_forgery with: :exception
+   helper_method :current_user, :logged_in?
+   def already_liked?
+     Like.where(user_id: current_user.id, article_id:
+     params[:article_id]).exists?
+   end
+  def current_user
+       @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+       !!current_user
+  end
+  
+  def require_user
+      if !logged_in?
+       flash[:error] = "You must be logged in to perform that action"
+       redirect_to root_path
+      end
+   end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-
+  root 'home#index'
   resources :posts do
     resources :comments
   end
-  
-  root 'posts#index'
-  
-  end
+resources :sessions, only: [:new, :create, :destroy]
+  get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+ get 'logout', to: 'sessions#destroy'
+  end 
